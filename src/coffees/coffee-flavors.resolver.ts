5a5cc155b4845132c7d68ea71a709cb9aad0d403
Ivan <ipykaloi@gmail.com>
2024-09-21 11:10:58 +0300

    data loader

--- src/coffees/coffee-flavors.resolver.ts
+++ src/coffees/coffee-flavors.resolver.ts
@@ -1,26 +1,14 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { Coffee } from './entities/coffee.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Flavor } from './entities/flavor.entity';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader';
 
 @Resolver(() => Coffee)
 export class CoffeeFlavorsResolver {
-  constructor(
-    // ⚙️ Inject the Flavor Repository
-    @InjectRepository(Flavor)
-    private readonly flavorsRepository: Repository<Flavor>,
-  ) {}
+  constructor(private readonly flavorsByCoffeeLoader: FlavorsByCoffeeLoader) {}
 
   @ResolveField('flavors', () => [Flavor])
   async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
-    // Using the injected repository,
-    // let’s retrieve ALL flavors that belong to a “parent coffee”.
-    return this.flavorsRepository
-      .createQueryBuilder('flavor')
-      .innerJoin('flavor.coffees', 'coffees', 'coffees.id = :coffeeId', {
-        coffeeId: coffee.id,
-      })
-      .getMany();
+    return this.flavorsByCoffeeLoader.load(coffee.id);
   }
 }
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,16 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { CoffeeFlavorsResolver } from './coffee-flavors.resolver';
 import { PubSubModule } from '../pub-sub/pub-sub.module';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor]), PubSubModule],
-  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorsResolver],
+  providers: [
+    CoffeesResolver,
+    CoffeesService,
+    CoffeeFlavorsResolver,
+    FlavorsByCoffeeLoader,
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
